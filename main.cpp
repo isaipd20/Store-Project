9c8ad1d37f7e4f589cc37295da09d2bf8c875449
Thezerolok <79038411+isaipd20@users.noreply.github.com>
2022-01-11 14:41:40 -0800

    show items and change items added

--- main.cpp
+++ main.cpp
@@ -25,6 +25,19 @@ void showItems() {
 
 }
 
+void changeItem(int num) {
+    cartValues x;
+
+    std::cin.ignore();
+    std::cout << "What item do you want to add to your cart?" << std::endl;
+    std::cout << "water \napples \nwatermelon \nchips \n";
+    std::getline(std::cin, x.item);
+    std::cout << "Enter quantity: ";
+    std::cin >> x.quantity;
+    cart[num].item = x.item;
+    cart[num].quantity = x.quantity;
+}
+
 void menu(int choise) {
     cartValues Cart;
 
@@ -57,6 +70,32 @@ void menu(int choise) {
     case 3:
         std::cout << "Thanks for shopping!!" << std::endl;
         break;
+
+    case 4:
+        int numItem;
+
+        if (cart.size() >= 1) {
+            std::cout << "What item do you want to change from your cart?" << std::endl;
+            showItems();
+            std::cin >> numItem;
+            changeItem(numItem);
+        }
+
+        else {
+            std::cout << "Your Cart is empty. Add items!!" << std::endl;
+        }
+        break;
+
+    case 5:
+
+        if (cart.size() >= 1) {
+            showItems();
+        }
+
+        else {
+            std::cout << "Your Cart is empty. Add items!!" << std::endl;
+        }
+        break;
     }
 }
 
@@ -66,7 +105,7 @@ int main()
 
     while (choise != 3) {
         std::cout << "Welcome to the store. Options available: " << std::endl;
-        std::cout << " 1: add item to cart \n 2: remove item from cart \n 3: quit \n";
+        std::cout << " 1: add item to cart \n 2: remove item from cart \n 3: quit \n 4: change item \n 5: show items \n";
         std::cin >> choise;
         menu(choise);
     }
